278a72674b97dc5cd236cc420612f5ebec0db466
NebelwerferT <NebelwerferT9@gmail.com>
2021-09-12 16:14:59 +0400

    feat: extended-repeater complete

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,26 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  let repCount = (options.repeatTimes !== undefined) ? options.repeatTimes : 1;
+  if (repCount <= 0) return str;
+  let sepStr = "";
+  let resSepStr = "";
+  let separator = "";
+  let resStr = str;
+  let addSep = "";
+  sepStr += (options.addition !== undefined) ? `${options.addition}` : "";
+  resSepStr += sepStr;
+  if (options.additionRepeatTimes > 0) {
+    addSep += (options.additionSeparator !== undefined) ? `${options.additionSeparator}` : "|";
+    for (let i = 1; i < options.additionRepeatTimes; i++) {
+      resSepStr += addSep + sepStr;
+    }
+  }
+  separator = (options.separator !== undefined) ? `${options.separator}` : "+";
+  resStr += resSepStr;
+  for (let i = 1; i < repCount; i++) {
+    resStr += separator + str + resSepStr;
+  }
+  return resStr;
 }

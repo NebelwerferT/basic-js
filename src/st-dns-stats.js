9309b0c26362871308328cd1b5f362f2d37ce381
NebelwerferT <NebelwerferT9@gmail.com>
2021-09-12 13:37:18 +0400

    feat: st-dns-stats complete

--- src/st-dns-stats.js
+++ src/st-dns-stats.js
@@ -22,7 +22,20 @@ import { NotImplementedError } from '../extensions/index.js';
  * }
  *
  */
-export default function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getDNSStats(domains) {
+  if (domains.length === 0) return {};
+  const arr = domains.map(elem => elem.split(".").map(elem => "." + elem).reverse());
+  const domainZone = "." + domains[0].split(".").pop();
+  const obj = {};
+  let curPath;
+  arr.forEach(elem => {
+    curPath = domainZone;
+    for (let i = 1; i < elem.length; i++) {
+      if (elem[i] === domainZone) continue;
+      curPath += elem[i];
+      (obj[curPath] === undefined) ? obj[curPath] = 1 : obj[curPath]++;
+    }
+  });
+  obj[domainZone] = Object.values(obj).length;
+  return obj;
 }
